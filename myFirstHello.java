791c247e302110aa5b56638e2dd0c4d18ebb8107
johnpatrick66 <johnpatrick66@yahoo.com>
2021-10-27 15:26:42 -0400

    Add files via upload

---
+++ myFirstHello.java
@@ -0,0 +1,11 @@
+package myFirstJavaProject;
+
+public class myFirstHello {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello there First World");
+	}
+
+
+}
